c45705b5f20962c731d613924fe55c970ee9acd9
Sheonna Harris <svonharris@gmail.com>
2026-04-29 13:04:55 -0400

    Changed the way pitchers are listed if names are not available.

--- src/components/UpcomingCard.tsx
+++ src/components/UpcomingCard.tsx
@@ -7,29 +7,19 @@ function UpcomingGame({ gameData }: UpcomingGameProps) {
     <div className="notstarted-game">
       <div>
         <h2>{gameData.teams.home.name}</h2>
-        {gameData.probablePitchers ? (
-          <p>
-            <span className="uppercase">pp: </span>
-            {gameData.probablePitchers.home?.fullName}
-          </p>
-        ) : (
-          <p>
-            <span className="uppercase">pp: </span>n/a
-          </p>
-        )}
+        <p>
+          <span className="uppercase">pp: </span>
+          {gameData.probablePitchers.home?.fullName ?? "n/a"}
+        </p>
       </div>
       <div>
         <h2>vs {gameData.teams.away.name}</h2>
-        {gameData.probablePitchers ? (
-          <div className="probable-pitchers">
-            <p>
-              <span className="uppercase">pp: </span>
-              {gameData.probablePitchers.away?.fullName}
-            </p>
-          </div>
-        ) : (
-          <p>n/a</p>
-        )}
+        <div className="probable-pitchers">
+          <p>
+            <span className="uppercase">pp: </span>
+            {gameData.probablePitchers.away?.fullName ?? "n/a"}
+          </p>
+        </div>
       </div>
       <div className="game-info">
         <p>
