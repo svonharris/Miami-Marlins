88c45c1650e81de2e075ae44eadec53c0f2a78f4
Sheonna Harris <svonharris@gmail.com>
2026-04-29 12:09:38 -0400

    Removed MlbApiCall component

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import GetRequestCalls from "./components/MlbApiCall.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <GetRequestCalls /> {/* Remove in production  */}
-  </StrictMode>
+  </StrictMode>,
 );
